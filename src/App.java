50370a00abd0a0fb3d405256f243929b24b99e56
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2025-01-11 12:48:24 +0100

    added loop for reading the repo list

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Collections;
 import java.util.Scanner;
 
@@ -6,15 +7,27 @@ public class App {
         char playerInput;
         Scanner input = new Scanner(System.in);
         Collections.shuffle(Repository.questionList);
+        int counter = 0;
         System.out.println("Bienvenido o bienvenida, para jugar, introduzca la letra a la izquierda de la pregunta.");                
         System.out.println("Introduzca su nombre de jugador");
         PlayerInfo.playerName = input.nextLine();
         System.out.println("Perfecto, "+PlayerInfo.playerName+". Empecemos con el juego...");
-        System.out.println(Repository.questionList.get(0).askPlayer());        
-        System.out.println(Repository.questionList.get(0).readAnswers());
-        playerInput = input.next().charAt(0);
-        System.out.println(Repository.questionList.get(0).checkAnswer(playerInput));
-        System.out.println(PlayerInfo.readScore());        
+        do {
+            System.out.println(Repository.questionList.get(counter).askPlayer());        
+            System.out.println(Repository.questionList.get(counter).readAnswers());
+            playerInput = input.next().charAt(0);
+            System.out.println(Repository.questionList.get(counter).checkAnswer(playerInput));
+            System.out.println(PlayerInfo.readScore());  
+            counter++;
+        } while (counter != Repository.questionList.size());
+
+        System.out.println("Ha obtenido un total de "+PlayerInfo.score);
+        if (PlayerInfo.score == Repository.questionList.size()) {
+            System.out.println("Felicidades, "+PlayerInfo.playerName+", ha alcanzado la puntuación máxima");
+        } else if (PlayerInfo.score == 0) {
+            System.out.println("Vaya, definitivamente hoy no era su día, "+PlayerInfo.playerName);
+        }
+             
         input.close();
     }
 }
