4c58a0055dc62acd98edb61ba625285f89f76b92
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2025-01-11 13:12:26 +0100

    added presenter for better readability

--- src/App.java
+++ src/App.java
@@ -1,33 +1,27 @@
-import java.sql.SQLOutput;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {        
+        Presenter presenter = new Presenter();
         char playerInput;
         Scanner input = new Scanner(System.in);
         Collections.shuffle(Repository.questionList);
         int counter = 0;
-        System.out.println("Bienvenido o bienvenida, para jugar, introduzca la letra a la izquierda de la pregunta.");                
-        System.out.println("Introduzca su nombre de jugador");
+        presenter.presentersSalute();
+        presenter.presenterAsksName();
         PlayerInfo.playerName = input.nextLine();
-        System.out.println("Perfecto, "+PlayerInfo.playerName+". Empecemos con el juego...");
+        presenter.presenterStartsGame();
         do {
-            System.out.println(Repository.questionList.get(counter).askPlayer());        
-            System.out.println(Repository.questionList.get(counter).readAnswers());
+            presenter.presenteAsksPlayer(counter);        
+            presenter.presenterReadsAnswers(counter);
             playerInput = input.next().charAt(0);
-            System.out.println(Repository.questionList.get(counter).checkAnswer(playerInput));
-            System.out.println(PlayerInfo.readScore());  
+            presenter.presenterChecksAnswers(counter, playerInput);
+            presenter.presenterTellsScore();
             counter++;
         } while (counter != Repository.questionList.size());
 
-        System.out.println("Ha obtenido un total de "+PlayerInfo.score);
-        if (PlayerInfo.score == Repository.questionList.size()) {
-            System.out.println("Felicidades, "+PlayerInfo.playerName+", ha alcanzado la puntuación máxima");
-        } else if (PlayerInfo.score == 0) {
-            System.out.println("Vaya, definitivamente hoy no era su día, "+PlayerInfo.playerName);
-        }
-             
+        presenter.presenterEndsShow();             
         input.close();
     }
 }
---
+++ src/Presenter.java
@@ -0,0 +1,43 @@
+public class Presenter {
+    public Presenter(){}
+
+    public void presentersSalute(){
+        System.out.println("Bienvenido o bienvenida, para jugar, introduzca la letra a la izquierda de la pregunta.");                
+    }
+
+    public void presenterAsksName(){
+        System.out.println("Introduzca su nombre de jugador");
+    }
+
+    public void presenterStartsGame(){
+        System.out.println(
+            "Perfecto, "+PlayerInfo.playerName+". Empecemos con el juego...\n"+
+            "Recuerde que para responder a las preguntas, tiene que seleccionar la respuesta con una letra de su teclado"
+        );
+    }
+
+    public void presenteAsksPlayer(int counter){
+        System.out.println(Repository.questionList.get(counter).askPlayer());
+    }
+
+    public void presenterReadsAnswers(int counter){
+        System.out.println(Repository.questionList.get(counter).readAnswers());
+    }
+
+    public void presenterChecksAnswers(int counter, char playerInput) {
+        System.out.println(Repository.questionList.get(counter).checkAnswer(playerInput));
+    }
+    
+    public void presenterTellsScore(){
+        System.out.println(PlayerInfo.readScore());
+    }
+
+    public void presenterEndsShow(){
+        System.out.println("Ha obtenido un total de "+PlayerInfo.score);
+        if (PlayerInfo.score == Repository.questionList.size()) {
+            System.out.println("Felicidades, "+PlayerInfo.playerName+", ha alcanzado la puntuación máxima");
+        } else if (PlayerInfo.score == 0) {
+            System.out.println("Vaya, definitivamente hoy no era su día, "+PlayerInfo.playerName);
+        }
+    }
+}
