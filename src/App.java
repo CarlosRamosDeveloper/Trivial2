0747e8595da6bd6a9b0ebf3af57c1143c66d89e1
CarlosRamosDeveloper <CarlosRamosDev@protonmail.com>
2025-01-11 11:40:58 +0100

    Added repository shuffle

--- src/App.java
+++ src/App.java
@@ -1,7 +1,10 @@
+import java.util.Collections;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(Repository.questionList.get(2).askPlayer());
-        System.out.println(Repository.questionList.get(2).readAnswers());
-        System.out.println(Repository.questionList.get(2).checkAnswer('a'));
+        Collections.shuffle(Repository.questionList);
+        System.out.println(Repository.questionList.get(0).askPlayer());
+        System.out.println(Repository.questionList.get(0).readAnswers());
+        System.out.println(Repository.questionList.get(0).checkAnswer('a'));
     }
 }
